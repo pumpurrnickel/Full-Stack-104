22fb2264b642c6fac4993ceb871c6e4753ff94bf
pumpurrnickel <denasanch1999@gmail.com>
2022-06-03 13:06:24 -0400

    didn't implement google oauth

---
+++ Springboo/oathen/src/main/java/com/springboot/oathen/OathenApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.oathen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OathenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OathenApplication.class, args);
+	}
+
+}
