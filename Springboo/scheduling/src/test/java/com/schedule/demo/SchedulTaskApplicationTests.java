d5d3948f9600fdf5a599997c6f45e5beb15779ae
pumpurrnickel <denasanch1999@gmail.com>
2022-06-05 22:56:48 -0400

    Completed pick ur own project

---
+++ Springboo/scheduling/src/test/java/com/schedule/demo/SchedulTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedule.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
